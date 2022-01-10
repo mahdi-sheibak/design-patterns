21661660e6c368986dd19ee2fe30fa15ee1df833
Mahdi zoraghi <mahdi.zoraghi.sheibak@gmail.com>
2022-01-10 21:51:42 +0330

    Feat: Strategy Pattern

---
+++ StrategyPattern/Strategy.js
@@ -0,0 +1,52 @@
+class UPS {
+  constructor() {
+    this.name = "UPS";
+  }
+
+  calculate() {
+    return 25;
+  }
+}
+
+class FedEx {
+  constructor() {
+    this.name = "FedEx";
+  }
+
+  calculate() {
+    return 30;
+  }
+}
+
+class DHL {
+  constructor() {
+    this.name = "DHL";
+  }
+
+  calculate() {
+    return 50;
+  }
+}
+
+class Shipping {
+  setStrategy(company) {
+    this.company = company;
+  }
+
+  calculate() {
+    return this.company.calculate();
+  }
+}
+
+const packageDelivery = { from: "76712", to: "10012", weight: "1kg" };
+
+const shipping = new Shipping();
+
+shipping.setStrategy(new UPS());
+console.log("UPS Strategy:", shipping.calculate(packageDelivery));
+
+shipping.setStrategy(new FedEx());
+console.log("FedEx Strategy:", shipping.calculate(packageDelivery));
+
+shipping.setStrategy(new DHL());
+console.log("DHL Strategy:", shipping.calculate(packageDelivery));
