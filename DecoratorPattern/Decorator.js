dd10fb02beb122bf62058af049f31bfe6ba5ab00
Mahdi zoraghi <mahdi.zoraghi.sheibak@gmail.com>
2022-01-10 14:59:39 +0330

    Feeat: Decorator Pattern

---
+++ DecoratorPattern/Decorator.js
@@ -0,0 +1,36 @@
+class User {
+  constructor(name) {
+    this.name = name;
+  }
+
+  getName() {
+    return this.name;
+  }
+
+  say() {
+    console.log(`My name is ${this.name}`);
+  }
+}
+
+class DecorateUser {
+  constructor(user, street, city) {
+    this.user = user;
+    this.street = street;
+    this.city = city;
+    this.name = user.name;
+  }
+
+  getName() {
+    return this.user.getName();
+  }
+
+  say() {
+    console.log(`Decorated User: ${this.name}, ${this.street}, ${this.city}`);
+  }
+}
+
+const user = new User("Mahdi");
+user.say(); // My name is Mahdi
+
+const decorated = new DecorateUser(user, "123 Main Street", "London");
+decorated.say(); // Decorated User: Mahdi, 123 Main Street, London
