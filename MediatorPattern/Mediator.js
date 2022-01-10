d3fb7ed58e60595bb3c1f30497c8d22da84c98a5
Mahdi zoraghi <mahdi.zoraghi.sheibak@gmail.com>
2022-01-10 17:31:47 +0330

    Feat: Mediator Pattern

---
+++ MediatorPattern/Mediator.js
@@ -0,0 +1,39 @@
+class Member {
+  constructor(name) {
+    this.name = name;
+    this.chatroom = null;
+  }
+
+  send(message, toMember) {
+    this.chatroom.send(message, this, toMember);
+  }
+
+  receive(message, fromMember) {
+    console.log(`${fromMember.name} to ${this.name}: ${message}`);
+  }
+}
+
+class Chatroom {
+  constructor() {
+    this.members = {};
+  }
+
+  addMember(member) {
+    this.members[member.name] = member;
+    member.chatroom = this;
+  }
+
+  send(message, fromMember, toMember) {
+    toMember.receive(message, fromMember);
+  }
+}
+
+const chatroom = new Chatroom();
+const mahdi = new Member("Mahdi");
+const john = new Member("John");
+
+chatroom.addMember(mahdi);
+chatroom.addMember(john);
+
+mahdi.send("How you doing?", john);
+john.send("Hey! Im good how are you yourself?", mahdi);
