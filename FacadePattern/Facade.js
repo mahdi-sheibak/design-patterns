a7404d951572cfb1b4a4801a8a36210e7475bc5d
Mahdi zoraghi <mahdi.zoraghi.sheibak@gmail.com>
2022-01-10 15:29:44 +0330

    Feat: Facade Pattern

---
+++ FacadePattern/Facade.js
@@ -0,0 +1,43 @@
+class Mortgage {
+  constructor(name) {
+    this.name = name;
+  }
+
+  apply(amount) {
+    let result = "approved";
+
+    if (!new Bank().get(this.name, amount)) {
+      result = "declined";
+    } else if (!new Credit().get(this.name)) {
+      result = "declined";
+    } else if (!new Background().get(this.name)) {
+      result = "declined";
+    }
+    return `${this.name} has been ${result} for a ${amount} mortgage`;
+  }
+}
+
+class Bank {
+  get(name, amount) {
+    // ...
+    return true;
+  }
+}
+
+class Credit {
+  get(name) {
+    // ...
+    return true;
+  }
+}
+
+class Background {
+  get() {
+    // ...
+    return true;
+  }
+}
+
+const mortgage = new Mortgage("Mahdi");
+const result = mortgage.apply(300000);
+console.log(result);
