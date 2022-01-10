7d36911eed0d1e8e73849af9820108b89028cde8
Mahdi zoraghi <mahdi.zoraghi.sheibak@gmail.com>
2022-01-10 18:14:14 +0330

    Feat: Observer Pattern

---
+++ ObserverPattern/Observer.js
@@ -0,0 +1,30 @@
+class Observer {
+  constructor() {
+    this.observers = []; // array of observer functions
+  }
+
+  subscribe(fn) {
+    this.observers.push(fn);
+  }
+
+  unsubscribe(fn) {
+    this.observers = this.observers.filter((f) => f !== fn);
+  }
+
+  fire() {
+    this.observers.forEach((observer) => observer());
+  }
+}
+
+function observer1() {
+  console.log("Observer 1 Fired!");
+}
+
+function observer2() {
+  console.log("Observer 2 Fired!");
+}
+
+const observer = new Observer();
+observer.subscribe(observer1);
+observer.subscribe(observer2);
+observer.fire();
