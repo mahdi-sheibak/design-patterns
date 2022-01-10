b5770b7dcb1cc25252c092e329b66e894f67b906
Mahdi zoraghi <mahdi.zoraghi.sheibak@gmail.com>
2022-01-10 21:14:36 +0330

    Feat: Singleton Pattern

---
+++ SingletonPattern/Singleton.js
@@ -0,0 +1,27 @@
+class Process {
+  constructor(state) {
+    this.state = state;
+  }
+}
+
+const Singleton = (function () {
+  let instance;
+
+  function createInstance() {
+    return new Process("New Object");
+  }
+
+  return {
+    getInstance: function () {
+      if (!instance) {
+        instance = createInstance();
+      }
+      return instance;
+    },
+  };
+})();
+
+const process1 = Singleton.getInstance();
+const process2 = Singleton.getInstance();
+
+console.log(process1 === process2); // true
